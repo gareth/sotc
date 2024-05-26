f4cf53d720cd7b63ce50f495654fa520e0151501
Gareth Adams <g@rethada.ms>
2024-05-26 13:39:14 +0100

    Add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,20 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const path = require("path");
 module.exports = {
+  mode: "development",
+
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  mode: "development",
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
