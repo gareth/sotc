2abf0fecac24a9a7cb902ca7cb5260fdde05eab4
Gareth Adams <g@rethada.ms>
2024-05-31 11:20:22 +0100

    Simplify scraper detection to pass raw data
    The intent is to do all the data interpretation in the worker

--- src/content.ts
+++ src/content.ts
@@ -6,7 +6,7 @@ logger.info("initialized");
 
 const port = chrome.runtime.connect({ name: "gameTab" });
 
-function _injectScript() {
+function injectScript() {
   const script = document.createElement("script");
   const runtimeUrl = chrome.runtime.getURL("inject.js");
   logger.debug("Injecting Vue monitor script", runtimeUrl);
@@ -24,11 +24,13 @@ function _injectScript() {
     });
   });
 
-  /* This never works, because the event is dispatched before the script loads.
-  Instead, we just rely on the injected script to notify us immediately (and
-  again whenever a change is detected) */
-  // logger.info("requesting Script");
-  // document.dispatchEvent(new CustomEvent("detectScript", {}));
+  document.addEventListener("sotc/script", (e: CustomEvent) => {
+    logger.info("Detected script change", e.detail);
+  });
+
+  document.addEventListener("sotc/gameState", (e: CustomEvent) => {
+    logger.info("Detected game state change", e.detail);
+  });
 }
 
-_injectScript();
+injectScript();
--- src/inject.ts
+++ src/inject.ts
@@ -1,18 +1,88 @@
 import { TaggedLogger } from "./util/TaggedLogger";
-import setupVueScraper from "./scrapers/VueScraper";
-import { watch } from "vue";
 
 type HTMLVueElement = HTMLElement & { __vue_app__: any };
 
 const logger = new TaggedLogger("Inject");
 logger.info("initialized");
 
+const IGNORED_MUTATIONS = [
+  "chat/updateServer",
+  "session/setPing",
+  "toggleModal",
+];
+
+type ScriptEvent = {
+  edition: unknown;
+  roles: unknown[];
+};
+
+type GameStateEvent = {
+  history: unknown;
+  phase: unknown;
+  isRunning: unknown;
+  isNight: unknown;
+};
+
+function inject(container: HTMLVueElement) {
+  const vueApp = container.__vue_app__;
+  const globals = vueApp._context.config.globalProperties;
+
+  logger.info("Adding Vue hooks");
+
+  logger.info("Adding script watcher");
+  const unwatchScript = globals.$store.watch(
+    (state) => [state.edition, state.roles],
+    ([edition, roles]) => {
+      logger.info("Detected VueX script change", { edition, roles });
+      const detail = JSON.parse(JSON.stringify({ edition, roles }));
+
+      document.dispatchEvent(
+        new CustomEvent<ScriptEvent>("sotc/script", { detail })
+      );
+    }
+  );
+
+  logger.info("Adding game watcher");
+  const unwatchGame = globals.$store.watch(
+    (state, getters) => [
+      state.game.history,
+      state.game.phase,
+      state.game.isRunning,
+      getters["game/isNight"],
+    ],
+    ([history, phase, isRunning, isNight]) => {
+      // const changes = oldValue.map((e, i) => [e, newValue[i]]);
+      const newState = { history, phase, isRunning, isNight };
+      logger.info("Detected VueX game change", newState);
+
+      const detail = JSON.parse(
+        JSON.stringify({
+          history,
+          phase,
+          isRunning,
+          isNight,
+        })
+      );
+      document.dispatchEvent(
+        new CustomEvent<GameStateEvent>("sotc/gameState", { detail })
+      );
+    }
+  );
+
+  logger.info("Adding mutation watcher");
+  const unsubscribe = globals.$store.subscribe((mutation, state) => {
+    if (IGNORED_MUTATIONS.includes(mutation.type)) return;
+
+    // logger.debug("VueX mutation", mutation, state);
+  });
+
+  logger.info("Adding route watcher");
+  globals.$router.afterEach((to, from) => {
+    logger.debug("Routing to", to, "from", from);
+  });
+}
+
 const main = document.getElementById("main") as HTMLVueElement;
-
-// setTimeout(() => {
-//   if (main.__vue_app__) {
-//     const globals = main.__vue_app__._context.config.globalProperties;
-//   }
-// }, 3000);
-
-setupVueScraper(main);
+if (main.__vue_app__) {
+  inject(main);
+}
