bc465e5cde31e1bb3766fe5ea881ae49d7c73d22
Gareth Adams <g@rethada.ms>
2024-07-08 17:24:15 +0100

    Extract game state to a separate component

--- src/twitch/OverlayApp.vue
+++ src/twitch/OverlayApp.vue
@@ -1,51 +1,51 @@
 <script async setup lang="ts">
-import { ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { TaggedLogger } from "../chrome/util/TaggedLogger";
 import { decode } from "../chrome/twitch/sync";
-import { ExtensionState } from "../chrome/types/sotc";
+import { ExtensionState, Script } from "../chrome/types/sotc";
+import { Seat } from "../chrome/types/event";
+import Game from "./Game.vue";
 
 const logger = new TaggedLogger("OverlayApp");
 
 const context = ref<object>({});
 
-const config = ref<Partial<ExtensionState>>({});
+const script = ref<Script | undefined>(undefined);
+const page = ref<string | undefined>(undefined);
+const seats = ref<Seat[] | undefined>(undefined);
+
+const config = computed(() => {
+  if (script.value && page.value && seats.value) {
+    return {
+      script: script.value,
+      page: page.value,
+      seats: seats.value,
+    };
+  } else {
+    return;
+  }
+});
 
 window.Twitch.ext.onContext((ctx) => {
   context.value = ctx;
 });
 
-window.Twitch.ext.configuration.onChanged(() => {
-  logger.info("Change detected");
-  if (Twitch.ext.configuration.broadcaster) {
-    const content = Twitch.ext.configuration.broadcaster.content;
-    const decompressed = decode(content) as Partial<ExtensionState>;
-    config.value = decompressed;
-  }
+onMounted(() => {
+  window.Twitch.ext.configuration.onChanged(() => {
+    if (Twitch.ext.configuration.broadcaster) {
+      const content = Twitch.ext.configuration.broadcaster.content;
+      const decompressed = decode(content) as Partial<ExtensionState>;
+      script.value = decompressed.script;
+      page.value = decompressed.page;
+      seats.value = decompressed.seats;
+    }
+  });
 });
-
-logger.info("Got initial config", config);
 </script>
 
 <template>
   <div class="overlay">
-    <h1>Stream on the Clocktower</h1>
-    <details v-if="config.script">
-      <summary v-text="config.script.name"></summary>
-      <div>by {{ config.script.author }}</div>
-      <ul>
-        <li v-for="role in config.script.characters" v-text="role.name"></li>
-      </ul>
-    </details>
-    <details open v-if="config.seats?.length">
-      <summary>Seats ({{ config.seats?.length }})</summary>
-      <ul>
-        <li v-for="player in config.seats">
-          <span v-if="player.user">{{ player.user }} </span>
-          <span v-else class="empty">[empty] </span>
-          <span v-if="player.role"> ({{ player.role?.name }})</span>
-        </li>
-      </ul>
-    </details>
+    <Game v-if="config" v-bind="config">1</Game>
   </div>
 </template>
 
