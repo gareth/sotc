25929168a5dd2086ea41c4fb1039e3924c51a44a
Gareth Adams <g@rethada.ms>
2024-07-29 00:53:00 +0100

    Comment Sentry out of Twitch overlay

--- src/twitch/overlay.ts
+++ src/twitch/overlay.ts
@@ -3,23 +3,23 @@ import "./overlay.scss";
 import OverlayApp from "./OverlayApp.vue";
 import { TaggedLogger } from "../chrome/util/TaggedLogger";
 
-import * as Sentry from "@sentry/vue";
-import { twitchExtensionDSN } from "../chrome/config/dsn";
+// import * as Sentry from "@sentry/vue";
+// import { twitchExtensionDSN } from "../chrome/config/dsn";
 
 const logger = new TaggedLogger("Overlay");
 logger.info("initialized");
 
 const app = createApp(OverlayApp);
 
-Sentry.init({
-  app,
-  dsn: twitchExtensionDSN,
-  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Session Replay
-  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+// Sentry.init({
+//   app,
+//   dsn: twitchExtensionDSN,
+//   integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+//   // Performance Monitoring
+//   tracesSampleRate: 1.0, //  Capture 100% of the transactions
+//   // Session Replay
+//   replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+//   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+// });
 
 app.mount("#app");
