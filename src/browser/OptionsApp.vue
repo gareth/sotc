645ea65fc0e925313fae23969b0f24180142f6a5
Gareth Adams <g@rethada.ms>
2024-08-05 17:38:07 +0100

    Improve twitch buttons on options page

--- src/browser/OptionsApp.vue
+++ src/browser/OptionsApp.vue
@@ -5,11 +5,6 @@ import { twitchAuth } from "./twitch/auth";
 import { twitchApi } from "./twitch/api";
 import { computed } from "vue";
 
-import {
-  setExtensionBroadcasterConfiguration,
-  getExtensionBroadcasterConfiguration,
-} from "@twurple/ebs-helper";
-
 const store = useLocalStore();
 
 const props = defineProps({
@@ -33,7 +28,7 @@ const props = defineProps({
 
 const logger = new TaggedLogger("Options");
 
-const doAuth = async (verify = false) => {
+async function connect(verify = false) {
   const data = await twitchAuth(verify).catch((e) => {
     logger.error("Authentication failed:", e);
     return undefined;
@@ -52,49 +47,53 @@ const doAuth = async (verify = false) => {
       store.id = { data: id };
     }
   }
-};
+}
 
-const ebsCallConfig = computed(() => {
-  return {
-    ownerId: props.ownerId,
-    clientId: props.clientId,
-    secret: props.secret,
-  };
-});
+function disconnect() {
+  store.auth = {};
+  store.id = {};
+}
 
 const api = computed(() =>
   store.accessToken ? twitchApi(store.accessToken) : undefined
 );
-
-const getInfo = async (api: ReturnType<typeof twitchApi> | undefined) => {
-  if (api) {
-    const newValue = Math.floor(Math.random() * 1000);
-    console.log("Setting new value", newValue);
-    await setExtensionBroadcasterConfiguration(
-      ebsCallConfig.value,
-      props.broadcasterId,
-      JSON.stringify({ random: newValue })
-    );
-    const data = await getExtensionBroadcasterConfiguration(
-      ebsCallConfig.value,
-      props.broadcasterId
-    );
-    if (data) {
-      logger.info("External config", JSON.parse(data.content));
-    }
-  }
-};
 </script>
 
 <template>
   <div>
     <h1>Stream on the Clocktower</h1>
     <h2>Options</h2>
-    <div v-text="store.id"></div>
-    <div v-text="api"></div>
-    <button @click="doAuth(false)">Connect with Twitch</button>
-    <button v-if="api" @click="getInfo(api)">Get info</button>
+    <h3>Twitch integration</h3>
+    <div v-if="store.id.data">
+      <div>
+        Connected as:
+        <strong>{{ store.id.data.preferred_username }}</strong>
+      </div>
+      <button @click="disconnect()">Disconnect</button>
+    </div>
+    <div v-else>
+      <button class="twitch" @click="connect(false)">
+        Connect with Twitch
+      </button>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+button.twitch {
+  background-color: #6441a5;
+  appearance: none;
+  border: 1px solid black;
+  padding: 0.6em 1.2em;
+  font-weight: bold;
+  color: white;
+
+  &:not(:disabled) {
+    cursor: pointer;
+  }
+
+  &:disabled {
+    background-color: grey;
+  }
+}
+</style>
